a2e3d6fec4292f1837f6df3ba92ccf0f0334c1c3
5FiftySix6 <ybham6@gmail.com>
2019-05-22 01:20:17 -0400

    Naming

--- MouseBindings.cs
+++ MouseBindings.cs
@@ -15,57 +15,48 @@ namespace QoL
         // F13, F14, F15
         // are 
         // Button4, Button5, Button6
-        // cause tc was high when they wrote this shit
         public override void Initialize()
         {
-            On.InputHandler.AddKeyBinding += TcWhy;
-            On.MappableKey.OnBindingAdded += Reeee;
+            On.InputHandler.AddKeyBinding += AddMouseBindings;
+            On.MappableKey.OnBindingAdded += ConvertMouseBindingToKey;
         }
 
         public void Unload()
         {
-            On.InputHandler.AddKeyBinding -= TcWhy;
-            On.MappableKey.OnBindingAdded -= Reeee;
+            On.InputHandler.AddKeyBinding -= AddMouseBindings;
+            On.MappableKey.OnBindingAdded -= ConvertMouseBindingToKey;
         }
 
-        private static void Reeee(On.MappableKey.orig_OnBindingAdded orig, MappableKey self, PlayerAction action, BindingSource binding)
+        private static void ConvertMouseBindingToKey(On.MappableKey.orig_OnBindingAdded orig, MappableKey self, PlayerAction action, BindingSource binding)
         {
-            // ReSharper disable once SwitchStatementMissingSomeCases
             switch (binding.Name)
             {
                 case "Button4":
                     action.AddBinding(new KeyBindingSource(Key.F13));
-
                     break;
                 case "Button5":
                     action.AddBinding(new KeyBindingSource(Key.F14));
-
                     break;
                 case "Button6":
                     action.AddBinding(new KeyBindingSource(Key.F15));
-
                     break;
             }
 
             orig(self, action, binding);
         }
 
-        private static void TcWhy(On.InputHandler.orig_AddKeyBinding orig, PlayerAction action, Key key)
+        private static void AddMouseBindings(On.InputHandler.orig_AddKeyBinding orig, PlayerAction action, Key key)
         {
-            // ReSharper disable once SwitchStatementMissingSomeCases
             switch (key)
             {
                 case Key.F13:
                     action.AddBinding(new MouseBindingSource(Mouse.Button4));
-
                     break;
                 case Key.F14:
                     action.AddBinding(new MouseBindingSource(Mouse.Button5));
-
                     break;
                 case Key.F15:
                     action.AddBinding(new MouseBindingSource(Mouse.Button6));
-
                     break;
             }
 
