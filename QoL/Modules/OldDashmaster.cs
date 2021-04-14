2d2df988fa678ab4dfc0793f6bae54a8ac83901b
fifty-six <ybham6@gmail.com>
2021-04-14 11:29:54 -0400

    Swap dashmaster changes to revert
    As 1.5 now has the old behaviour of 'FixDashmaster' by default, we now
    offer 'OldDashmaster', which keeps the behaviour of previous versions

---
+++ QoL/Modules/OldDashmaster.cs
@@ -0,0 +1,40 @@
+﻿using InControl;
+using JetBrains.Annotations;
+using MonoMod.Cil;
+
+namespace QoL.Modules
+{
+    [UsedImplicitly]
+    public class OldDashmaster : FauxMod
+    {
+        public OldDashmaster() : base(false) { }
+
+        public override void Initialize()
+        {
+            Unload();
+
+            IL.HeroController.HeroDash += HeroDash;
+        }
+
+        private void HeroDash(ILContext il)
+        {
+            var cursor = new ILCursor(il);
+
+            while (cursor.TryGotoNext
+            (
+                i => i.MatchLdloc(0),
+                i => i.MatchLdfld<HeroActions>("left") || i.MatchLdfld<HeroActions>("right"),
+                i => i.MatchCallvirt<OneAxisInputControl>("get_IsPressed"),
+                i => i.MatchBrtrue(out _)
+            ))
+            {
+                cursor.RemoveRange(4);
+            }
+        }
+
+        public override void Unload()
+        {
+            IL.HeroController.HeroDash -= HeroDash;
+        }
+    }
+}
